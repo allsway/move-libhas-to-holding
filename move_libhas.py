881e28dbeae9bc16f9ade9225ef0906ba23060bd
allsway <ssinick@calstate.edu>
2017-07-08 01:01:51 -0700

    Added source marc tag and target marc tag configurability, tested on set

---
+++ move_libhas.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+import requests
+import sys
+import csv
+import configparser
+import logging
+import xml.etree.ElementTree as ET
+
+# Returns the API key
+def get_key():
+	return config.get('Params', 'apikey')
+
+# Returns the Alma API base URL
+def get_base_url():
+	return config.get('Params', 'baseurl')
+
+def get_marc_field():
+	return config.get('Params', 'marc_tag')
+
+def get_target_marc_field():
+	return config.get('Params', 'target_marc_tag')
+
+# Return holding API url
+def get_holding_url(mms_id,holding_id):
+	return get_base_url() + "bibs/" + mms_id + '/holdings/' + holding_id + '?apikey=' + get_key()
+
+"""
+	Creates the new marc datafield element and appends all subfields from the 891 field to the 853 holding field
+"""
+def add_marc_field(record,new_subfields):
+	marc = ET.Element('datafield')
+	marc.set("tag",get_target_marc_field())
+	for key,value in new_subfields:
+		sub = ET.SubElement(marc,'subfield')
+		sub.set('code', 'a')
+		sub.text = value
+	record.append(marc)
+	return record
+
+"""
+	Gets holding data form the Alma API, calls , and posts updated holding with added 853 field
+"""
+def create_libhas_field(url,new_subfields):
+	response = requests.get(url)
+	if response.status_code != 200:
+		return None
+	holding = ET.fromstring(response.content)
+	new_subfields = sorted(new_subfields.items())
+	record = holding.findall('record')[0]
+	record = add_marc_field(record,new_subfields)
+	headers = {"Content-Type": "application/xml"}
+	r = requests.put(url,data=ET.tostring(holding),headers=headers)
+	print (r.content)
+	if r.status_code == 200:
+		logging.info('Successful update for ' + url)
+
+# Gets all subfields from the XXX MARC field
+def get_marc_elements(datafield):
+	new_subfields = {}
+	for subfields in datafield.findall('subfield'):
+		# Let's throw out the $9LOCAL - there's no need to carry it over to the holding
+		if subfields.attrib['code'] != '9':
+			new_subfields[subfields.attrib['code']] = subfields.text
+	return new_subfields
+
+"""
+	Read in bib record XML export from Alma
+"""
+def read_bibs(bib_records):
+	tree = ET.parse(bib_records)
+	for records in tree.findall('record'):
+		# Get bib record MMS ID
+		mms_id = records.find('./controlfield[@tag="001"]').text
+		logging.info('Bib MMS ID: ' + mms_id)
+		# get each lib has field
+		for libhas in records.findall('./datafield[@tag="' + get_marc_field() + '"]'):
+			new_subfields = get_marc_elements(libhas)
+			for holding in records.findall('./datafield[@tag="852"]'):
+				holding_id = holding.find('subfield[@code="8"]').text
+				print (holding_id)
+				if holding_id is not None:
+					url = get_holding_url(mms_id,holding_id)
+					# add the 853 field to the holing, with the 891 subfields
+					create_libhas_field(url,new_subfields)
+				else:
+					logging.info('No holding found in record: ' +  mms_id)
+		else:
+			logging.info('No 891 field found in record: ' + mms_id)
+
+
+
+logging.basicConfig(filename='status.log',level=logging.DEBUG)
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
+bib_recs = sys.argv[2]
+read_bibs(bib_recs)
