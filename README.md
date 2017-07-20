5f4e27b3850c9452db9e75066472f1d9013cf373
allsway <ssinick@calstate.edu>
2017-07-19 17:31:59 -0700

    Test message

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # move-libhas-to-holding
 Moves a MARC 95X-999 field that contains the LIB HAS information from the bib record to an associated holding
+TEST EDIT FOR KIMMIE
--- move_libhas.py
+++ move_libhas.py
@@ -4,6 +4,7 @@ import sys
 import csv
 import configparser
 import logging
+import json
 import xml.etree.ElementTree as ET
 
 # Returns the API key
@@ -41,12 +42,16 @@ def add_marc_field(record,new_subfields):
 	Gets holding data form the Alma API, calls , and posts updated holding with added 853 field
 """
 def create_libhas_field(url,new_subfields):
+	match = False
 	response = requests.get(url)
 	if response.status_code != 200:
 		return None
 	holding = ET.fromstring(response.content)
 	new_subfields = sorted(new_subfields.items())
 	record = holding.findall('record')[0]
+	print (record)
+	# change this to compare the data t othe new_subfields data
+
 	record = add_marc_field(record,new_subfields)
 	headers = {"Content-Type": "application/xml"}
 	r = requests.put(url,data=ET.tostring(holding),headers=headers)
@@ -63,9 +68,19 @@ def get_marc_elements(datafield):
 			new_subfields[subfields.attrib['code']] = subfields.text
 	return new_subfields
 
-"""
-	Read in bib record XML export from Alma
-"""
+# Calls the holdings API to get the correct sort order
+def get_last_holding(mms_id):
+	url = get_base_url() + "bibs/" + mms_id + '/holdings?apikey=' + get_key()
+	response = requests.get(url)
+	if response.status_code == 200:
+		holdings = ET.fromstring(response.content)
+		record_count = int(holdings.get('total_record_count'))
+		if record_count > 0:
+			holding_id =  holdings.findall('./holding/holding_id')[-1]
+			print (holding_id.text)
+			return holding_id.text
+
+# Read in bib record XML export from Alma
 def read_bibs(bib_records):
 	tree = ET.parse(bib_records)
 	for records in tree.findall('record'):
@@ -75,15 +90,17 @@ def read_bibs(bib_records):
 		# get each lib has field
 		for libhas in records.findall('./datafield[@tag="' + get_marc_field() + '"]'):
 			new_subfields = get_marc_elements(libhas)
-			for holding in records.findall('./datafield[@tag="852"]'):
-				holding_id = holding.find('subfield[@code="8"]').text
-				print (holding_id)
-				if holding_id is not None:
-					url = get_holding_url(mms_id,holding_id)
-					# add the 853 field to the holing, with the 891 subfields
-					create_libhas_field(url,new_subfields)
-				else:
-					logging.info('No holding found in record: ' +  mms_id)
+			print (new_subfields)
+		#	for holding in records.findall('./datafield[@tag="852"]'):
+		# Instead of getting the holding ID from the export, call the /holdings API
+			holding_id = get_last_holding(mms_id)
+			print (holding_id)
+			if holding_id is not None:
+				url = get_holding_url(mms_id,holding_id)
+				# add the 853 field to the holing, with the 891 subfields
+				create_libhas_field(url,new_subfields)
+			else:
+				logging.info('No holding found in record: ' +  mms_id)
 		else:
 			logging.info('No 891 field found in record: ' + mms_id)
 
